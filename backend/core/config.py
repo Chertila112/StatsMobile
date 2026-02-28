ec658c2f0a7162e49c2ef30a7ff1379c76e67855
kikbutt <sukhin636@gmail.com>
2026-02-28 22:14:28 +0300

    QoL change to pathing

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,18 +1,19 @@
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv("/home/kikbutt/Projects/StatsMobile/backend/venv/env/API_KEY.env")
+# Путь к .env относительно текущего скрипта
+dotenv_path = Path(__file__).parent.parent / "API_KEY.env"
+load_dotenv(dotenv_path)
+
 
 class Settings:
     def __init__(self):
         self.RIOT_API_KEY = os.getenv("RIOT_API_KEY")
 
-    def get_riot_headers(self) -> dict[str, str]:
-        if not self.RIOT_API_KEY:
-            raise RuntimeError("RIOT_API_KEY is missing in environment variables")
+    def get_riot_headers(self) -> dict[str, str | None]:
+        return {"X-Riot-Token": self.RIOT_API_KEY}
 
-        return {
-            "X-Riot-Token": self.RIOT_API_KEY
-        }
 
 settings = Settings()
+print(settings.get_riot_headers())
